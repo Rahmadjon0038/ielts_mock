b188a9b5b910953f6892ddceef371113fa23d35d
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2025-07-26 12:41:33 +0500

    fix

--- src/app/StyledComponentsRegistry.jsx
+++ src/app/StyledComponentsRegistry.jsx
@@ -1,17 +1,8 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { useServerInsertedHTML } from 'next/navigation';
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
+import React from "react";
+import { StyleSheetManager } from "styled-components";
 
 export default function StyledComponentsRegistry({ children }) {
-  const [sheet] = React.useState(() => new ServerStyleSheet());
-
-  useServerInsertedHTML(() => {
-    const styles = sheet.getStyleElement();
-    sheet.instance.clearTag(); // bu oldingi style'larni tozalaydi
-    return <>{styles}</>;
-  });
-
-  return <StyleSheetManager sheet={sheet.instance}>{children}</StyleSheetManager>;
+  return <StyleSheetManager>{children}</StyleSheetManager>;
 }
