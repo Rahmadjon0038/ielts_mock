ac88769dd002419a21e87d14a5f438e340df4c56
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2025-11-23 20:12:00 +0500

    sa

--- src/hooks/api.jsx
+++ src/hooks/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 export const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://apiielts.animall.uz/",
 });
 
 instance.interceptors.request.use(
