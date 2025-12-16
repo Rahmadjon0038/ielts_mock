4db8fac7f59bcc53333bba322199412ee0f18407
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2025-12-17 02:12:38 +0500

    nimadur

--- src/app/StyledComponentsRegistry.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import { StyleSheetManager } from "styled-components";
-
-export default function StyledComponentsRegistry({ children }) {
-  return <StyleSheetManager>{children}</StyleSheetManager>;
-}
